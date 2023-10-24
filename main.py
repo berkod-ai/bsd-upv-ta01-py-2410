94b1fea2ea51b0c90f4c43b960be8e95f99c979e
Berkod-AI <berkodk.ai@gmail.com>
2023-10-24 20:01:56 +0200

    eje2 done

--- main.py
+++ main.py
@@ -8,6 +8,17 @@ def eje1():
         print("Esperar")
 
 
+def eje2():
+    compra = int(input("Cuanto es la compra?: "))
+    if compra > 100:
+        descuento = compra*0.9
+        precio = compra - descuento
+    else:
+        precio = compra
+    print(precio)
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    eje1()
+    #eje1()
+    eje2()
